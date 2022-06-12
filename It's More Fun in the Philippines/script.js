b3db5585213fcec2f77372adec49b0b372132d96
engroliver <engr.oliver@icloud.com>
2022-06-12 08:00:37 +0000

    Assignment Save

--- It's More Fun in the Philippines/script.js
+++ It's More Fun in the Philippines/script.js
@@ -2,6 +2,19 @@ function main (){
 function init(){
 let map = initMap();
 
+    window.addEventListener("DOMContentLoaded",function(){
+
+        document.querySelector('#btnSearch').addEventListener('click',async function(){
+            let query = document.querySelector('#textInput').value;
+            let center = map.getBounds().getCenter();
+            let result = await search (center.lat,center.lng, query);
+            console.log(result)
+        })
+
+
+
+})
+
 }
 
 init();
